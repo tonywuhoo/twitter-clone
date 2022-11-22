beee41635bbaa9acf950c7ecc3975c79718d6eae
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-22 14:54:21 -0500

    created footer.jsx and moved modal signup/signin buttons to component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,17 @@ import "./App.css";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widget from "./Widget";
-
-
-
+import Footer from "./Footer";
 
 function App() {
- 
   return (
     // BEM
     <div className="app">
       <h1>Twotter</h1>
-
-      
       <Sidebar />
       <Feed />
-
       {/* Widget if we decide to popular */}
-
       <Widget />
-
     </div>
   );
 }
