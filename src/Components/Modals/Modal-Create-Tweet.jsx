67a1037b2617da02dfe2c507ca60fa48045b1176
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-11-22 14:32:22 -0500

    file name change

---
+++ src/Components/Modals/Modal-Create-Tweet.jsx
@@ -0,0 +1,16 @@
+// import ".Modal.css"
+
+// function Modal(props) {
+//   if (!props.show) {
+//     return null
+//   }
+//   return ( 
+//     <div className="modal">
+//       <div className="modal-content">
+//         <div className="modal=header">
+//           <h4 className="modal-title">
+            
+//           </h4>
+//         </div>
+//       </div>
+//     </div>
