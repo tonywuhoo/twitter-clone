5ac0a808f9ac2eb4f693af917743b756caf484a7
Suji Falcon <suji.falcon@gmail.com>
2022-11-22 13:56:21 -0500

    started routes for pages and installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from "react";
-import "./App.css";
-import Sidebar from "./Sidebar";
-import Feed from "./Feed";
-import Widget from "./Widget";
-
-
-
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import Widget from './Widget';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
- 
-  return (
-    // BEM
-    <div className="app">
-      <h1>Twotter</h1>
+	return (
+		// BEM
+		<div className='app'>
+			<h1>Twotter</h1>
 
-      
-      <Sidebar />
-      <Feed />
+			<Sidebar />
+			<Routes></Routes>
+			<Feed />
 
-      {/* Widget if we decide to popular */}
+			{/* Widget if we decide to popular */}
 
-      <Widget />
-
-    </div>
-  );
+			<Widget />
+		</div>
+	);
 }
 
 export default App;
