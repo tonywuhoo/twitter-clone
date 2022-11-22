196bffc663046e54af9fae5e16d4182fcfedc201
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-22 16:38:21 -0500

    fixed widgets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 // import ProfilePage from './Components/Profile/ProfilePage';
 // import { Routes, Route } from 'react-router-dom';
 import React from "react";
@@ -6,7 +5,7 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import Feed from "./Screens/Feed";
 import Widget from "./Components/Widget";
-// import Footer from "./Components/Footer";
+import Footer from "./Components/Footer";
 
 function App() {
   // useEffect(() => {
@@ -22,16 +21,13 @@ function App() {
   return (
     // BEM
     <div className="app">
-
       <h1>$hill</h1>
       <Sidebar />
       <Feed />
-
-      
-      
       {/* Widget if we decide to popular */}
 
       <Widget />
+      <Footer />
     </div>
   );
 }
