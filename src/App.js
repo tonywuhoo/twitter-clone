8360e0b1c58a4535700bb3a7ba30048bbaced909
Suji Falcon <suji.falcon@gmail.com>
2022-11-22 16:09:42 -0500

    started profile page and routes again

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import "./App.css";
-import Sidebar from "./Sidebar";
-import Feed from "./Feed";
-import Widget from "./Widget";
-import Footer from "./Footer";
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import Widget from './Widget';
+import Footer from './Footer';
+import ProfilePage from './Components/Profile/ProfilePage';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    // BEM
-    <div className="app">
-      <h1>Twotter</h1>
-      <Sidebar />
-      <Routes>
-				<Route path='/profile' element={<Profile />} />
+	return (
+		// BEM
+		<div className='app'>
+			<h1>Twotter</h1>
+			<Sidebar />
+			<Routes>
+				<Route path='/profile' element={<ProfilePage />} />
 			</Routes>
-      <Feed />
-      {/* Widget if we decide to popular */}
-      <Widget />
-      <Footer />
-    </div>
-  );
+			<Feed />
+			{/* Widget if we decide to popular */}
+			<Widget />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Profile/ProfilePage.jsx
@@ -0,0 +1,4 @@
+// import Sidebar from '../../Sidebar';
+// import SidebarOption from '../../SidebarOption';
+// import Feed from '../../Feed';
+// import Widget from '../../Widget';
