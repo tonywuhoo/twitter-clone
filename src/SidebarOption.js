07623b0976ed2d43ecbff583186150cb1d2e940e
Suji Falcon <suji.falcon@gmail.com>
2022-11-22 13:40:11 -0500

    moved sign-in and sign-up modal code from sidebaroption.js to widget.js

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,28 +1,18 @@
-import React from "react";
-import "./SidebarOption.css";
-// import SignUpModal from "./Components/Modals/Modal-Sign-Up";
-// import Modal from "./Components/Modals/Modal-Sign-In";
+import React from 'react';
+import './SidebarOption.css';
 // import { useState } from "react";
 
 function SidebarOption({ active, text, Icon }) {
-  // const [showModal, setShowModal] = useState(false);
-  return (
-    <>
-      <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
-        {Icon}
+	// const [showModal, setShowModal] = useState(false);
+	return (
+		<>
+			<div className={`sidebarOption ${active && 'sidebarOption--active'}`}>
+				{Icon}
 
-        <h2>{text}</h2>
-      </div>
-      {/* <div className="sign-in">
-        <Modal onClose={() => setShowModal(false)} show={showModal} />
-        <button onClick={() => setShowModal(true)}>Sign in!</button>
-      </div>
-      <div>
-        <SignUpModal onClose={() => setShowModal(false)} show={showModal} />
-        <button onClick={() => setShowModal(true)}>Sign up!</button>
-      </div> */}
-    </>
-  );
+				<h2>{text}</h2>
+			</div>
+		</>
+	);
 }
 
 export default SidebarOption;
