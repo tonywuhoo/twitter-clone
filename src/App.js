06e60be3930b580fcf1528c30968208c03c6359c
Suji Falcon <suji.falcon@gmail.com>
2022-11-28 11:34:41 -0500

    i didn't change anything but it says i did :shrug:

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 // import ProfilePage from './Components/Profile/ProfilePage';
 // import { Routes, Route } from 'react-router-dom';
-import React, { useState, useEffect} from "react";
-import "./App.css";
-import Sidebar from "./Components/Sidebar";
-import Feed from "./Screens/Feed";
-import Widget from "./Components/Widget";
-import Home from "./Screens/Home";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Sidebar from './Components/Sidebar';
+import Feed from './Screens/Feed';
+import Widget from './Components/Widget';
+import Home from './Screens/Home';
 
 function App() {
-  // useEffect(() => {
-  //   const callApi = async () => {
-  //     const response = await getArtists();
-  //     setArtists(response);
-  //     const res = await getAlbums();
-  //     setAlbums(res);
-  //   };
-  //   callApi();
-  // }, [toggleApiCall]);
+	// useEffect(() => {
+	//   const callApi = async () => {
+	//     const response = await getArtists();
+	//     setArtists(response);
+	//     const res = await getAlbums();
+	//     setAlbums(res);
+	//   };
+	//   callApi();
+	// }, [toggleApiCall]);
 
-  return (
-    // BEM
-    <div className="app">
-      <Home />
-      {/* <Sidebar />
+	return (
+		// BEM
+		<div className='app'>
+			<Home />
+			{/* <Sidebar />
       <Feed /> */}
-      {/* Widget if we decide to popular */}
+			{/* Widget if we decide to popular */}
 
-      {/* <Widget /> */}
-    </div>
-  );
+			{/* <Widget /> */}
+		</div>
+	);
 }
 
 export default App;
