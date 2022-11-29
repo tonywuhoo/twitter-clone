563640694245cc4157eb1fe8ba0a9456c031b7d3
Suji Falcon <suji.falcon@gmail.com>
2022-11-29 15:59:18 -0500

    added blur to modal background. added functionality to be able to click anywhere to close modal

--- src/Components/Modals/Modal-Sign-Up.jsx
+++ src/Components/Modals/Modal-Sign-Up.jsx
@@ -1,29 +1,31 @@
-import "./Modal.css"
-import Button from '@mui/material/Button';
+import "./Modal.css";
+import Button from "@mui/material/Button";
 
 function SignUpModal(props) {
-  if (!props.show) {
-    return null
-  }
-  
-  return (
-    <div className="modal-signup" onClick={props.onClose}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <div className="modal-header">
-        <div className="modal-exit">
-          </div>
-          <h1 className="modal-title">Sign Up</h1>
-          <input className='modal-input' placeholder='Create Username'></input>
-          <input className='modal-input' placeholder='Email'></input>
-          <input className='modal-input' placeholder=' Password'></input>
-          <input className='modal-input2' placeholder='Profile Description'></input>
-          <Button className="modal-button">Sign Up</Button>
-          <Button onClick={props.onClose} className="modal-button2">Close</Button>
-          </div>
-        </div>
-      </div>
-  )
+	if (!props.show) {
+		return null;
+	}
 
+	return (
+		<div className="modal" onClick={props.onClose}>
+			<div className="modal-content">
+				<div className="modal-header" onClick={(e) => e.stopPropagation()}>
+					<div className="modal-exit"></div>
+					<h1 className="modal-title">Sign Up</h1>
+					<input className="modal-input" placeholder="Create Username"></input>
+					<input className="modal-input" placeholder="Email"></input>
+					<input className="modal-input" placeholder=" Password"></input>
+					<input
+						className="modal-input2"
+						placeholder="Profile Description"></input>
+					<Button className="modal-button">Sign Up</Button>
+					<Button onClick={props.onClose} className="modal-button2">
+						Close
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default SignUpModal
+export default SignUpModal;
