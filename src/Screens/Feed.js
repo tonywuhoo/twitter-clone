1a7dfd085696162d966f347eeea821df5b732302
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-30 17:31:15 -0500

    remove feed.jserrors

--- src/Screens/Feed.js
+++ src/Screens/Feed.js
@@ -1,40 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "./Feed.css";
-import { getPosts } from "../services/PostCrud"
+import { getPosts } from "../services/PostCrud";
 import Tweetbox from "../Components/Tweetbox.jsx";
 import Post from "../Components/Post.jsx";
 
 function Feed() {
-
-  const [post, setPosts] = useState()
+  const [post, setPosts] = useState();
 
   useEffect(() => {
-
     async function grabPosts() {
-      let response = await getPosts()
-      await setPosts(response.data)
-      console.log(response.data)
+      let response = await getPosts();
+      await setPosts(response.data);
+      console.log(response.data);
     }
-    
-  grabPosts()
-  }, []);
-  
 
-	return (
-		<div className="feed-container">
-			<div className="feed">
-				<div className="feed-header">
-					<h2>Home Page</h2>
-				</div>
-				<Tweetbox />
-        <Post post={post}/>
-			</div>
-				<Tweetbox />
-				<Post />
+    grabPosts();
+  }, []);
+
+  return (
+    <div className="feed-container">
+      <div className="feed">
+        <div className="feed-header">
+          <h2>Home Page</h2>
         </div>
-        </div>
-		</div>
-	);
+        <Tweetbox />
+        <Post post={post} />
+      </div>
+      <Tweetbox />
+      <Post />
+    </div>
+  );
 }
 
 export default Feed;
