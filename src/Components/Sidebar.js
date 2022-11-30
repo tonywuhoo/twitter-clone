fb6ab42b3150b847357c95aabf6c6c9ecaf227bc
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-30 11:49:29 -0500

    start styling mobile nav bar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import "./Sidebar.css";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import SidebarOption from "./SidebarOption";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
@@ -12,9 +11,7 @@ import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import SmartButtonIcon from "@mui/icons-material/SmartButton";
 import Logo from ".././Components/shill.png";
-
 import ModalCreateTweet from "./Modals/Modal-Create-Tweet";
-
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import { ImageOutlined } from "@mui/icons-material";
 
@@ -25,12 +22,13 @@ function Sidebar() {
     <div className="sidebar-container">
       <div className="sidebar">
         <img
-          className="profile-photo"
+          className="sidebar-profile-photo"
           src={Logo}
           alt={"Shill Site"}
           width="80px"
         />
-        <SidebarOption
+          <SidebarOption
+            className="side-bar-icon"
           active
           Icon={<HomeIcon />}
           text="$hill"
