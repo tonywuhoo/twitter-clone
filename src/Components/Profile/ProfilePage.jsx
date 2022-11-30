c2e4ca27fa1fddfd462d9ba0addf44f728b2dce8
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-30 15:01:50 -0500

    media queries and react router

--- src/Components/Profile/ProfilePage.jsx
+++ src/Components/Profile/ProfilePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Widget from "../Widget";
+import "./ProfilePage.module.css"
 
 function ProfilePage() {
   return (
   <>
-  <h1>Profile Page</h1>
+      <h1>Profile Page</h1>
+      
+      <div className="profile-page-widget-container">
       <Widget /> 
+      </div>
       </>
   )
 } 
