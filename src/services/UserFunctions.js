c38d606b84b048aeaf2fc65d05cb2534f32d63fe
Suji Falcon <suji.falcon@gmail.com>
2022-12-01 15:27:39 -0500

    made it so sign up modal closes and reloads page on submit. have it so that if you try to register a new user it will reload the page and be logged in to the account IDEALLY but instead it logs in to the previously created account.

--- src/services/UserFunctions.js
+++ src/services/UserFunctions.js
@@ -1,28 +1,34 @@
 import axios from "axios";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
 export const doLogin = async (credentials) => {
-  try {
-    const response = await axios.post("https://twitter-clone-backend-production-c9cc.up.railway.app/auth/login/", credentials);
-    return response
-  } catch (error) {
-    throw error;
-  }
-}
+	try {
+		const response = await axios.post(
+			"https://twitter-clone-backend-production-c9cc.up.railway.app/auth/login/",
+			credentials,
+		);
+		return response;
+	} catch (error) {
+		throw error;
+	}
+};
 
 export const doRegister = async (credentials) => {
-  console.log("Works")
-  console.log(credentials)
-  try {
-    const response = await axios.post("https://twitter-clone-backend-production-c9cc.up.railway.app/auth/register/", credentials);
-    console.log(response.status)
-    return response
-  } catch (error) {
-    throw error;
-  }
+	try {
+		const response = await axios.post(
+			"https://twitter-clone-backend-production-c9cc.up.railway.app/auth/register/",
+			credentials,
+		);
+		console.log(response.status);
+		Cookies.set("AccessToken", "loggedin");
+		window.location.reload();
+		return response;
+	} catch (error) {
+		throw error;
+	}
 };
 
 export const doLogout = async () => {
-  Cookies.set("AccessToken", "loggedout")
-  window.location.reload()
-}
+	Cookies.set("AccessToken", "loggedout");
+	window.location.reload();
+};
