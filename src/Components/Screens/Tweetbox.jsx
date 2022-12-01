1b5114b2b18fbf94f765b991a8f90308aa9c1895
jscsonka <113357554+jscsonka@users.noreply.github.com>
2022-12-01 11:04:50 -0500

    muted image url input

--- src/Components/Screens/Tweetbox.jsx
+++ src/Components/Screens/Tweetbox.jsx
@@ -9,63 +9,64 @@ import { useState } from "react";
 import Cookies from "js-cookie";
 
 function Tweetbox() {
-	const [imageURL, setImageURL] = useState("");
-	const [text, setText] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [text, setText] = useState("");
 
-	const doSubmit = async (event) => {
-		event.preventDefault();
-		try {
-			const content = {
-				text: text,
-				title: imageURL,
-			};
-			await createPost(content);
-		} catch (error) {
-			throw error;
-		}
-	};
+  const doSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const content = {
+        text: text,
+        title: imageURL,
+      };
+      await createPost(content);
+    } catch (error) {
+      throw error;
+    }
+  };
 
-	const handleChange = (event) => {
-		if (event.target.id === "text") {
-			setText(event.target.value);
-		}
-		if (event.target.id === "image") {
-			setImageURL(event.target.value);
-		}
-	};
+  const handleChange = (event) => {
+    if (event.target.id === "text") {
+      setText(event.target.value);
+    }
+    if (event.target.id === "image") {
+      setImageURL(event.target.value);
+    }
+  };
 
-	return (
-		<div className="tweetBox">
-			<form onSubmit={doSubmit}>
-				<div className="tweetBox-input">
-					<textarea
-						id="text"
-						className="tweetBox-input"
-						placeholder="What's happening, tweed?"
-						type="text"
-						size="2rem"
-						onChange={handleChange}
-					/>
-				</div>
-				<input
+  return (
+    <div className="tweetBox">
+      <form onSubmit={doSubmit}>
+        <div className="tweetBox-input">
+          <textarea
+            id="text"
+            className="tweetBox-input"
+            placeholder="What's happening, tweed?"
+            type="text"
+            size="2rem"
+            onChange={handleChange}
+          />
+        </div>
+        {/* <input
 					id="image"
 					className="tweetBox-inputImage"
 					placeholder="Enter image URL"
 					type="text"
 					onChange={handleChange}
-				/>
+				/> */}
 
-				<Button
-					variant="outlined"
-					className="feed_tweet_BTN"
-					type="submit"
-					// onClick={sendTweet}
-					fullWidth>
-					Post
-				</Button>
-			</form>
-		</div>
-	);
+        <Button
+          variant="outlined"
+          className="feed_tweet_BTN"
+          type="submit"
+          // onClick={sendTweet}
+          fullWidth
+        >
+          Post
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default Tweetbox;
