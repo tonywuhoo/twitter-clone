2b43f759b34b92090a714aed77b800f243d72cbf
gary bowen <garybowen@garys-MacBook-Air.local>
2022-12-07 09:36:08 -0500

    nav done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,77 +1,92 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
-import SidebarOption from "./Components/SidebarOptions/SidebarOption";
-import HomeIcon from "@mui/icons-material/Home";
+
 import Button from "@mui/material/Button";
 import ListAltIcon from "@mui/icons-material/ListAlt";
-import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+
 import Logo from "./shill.png";
 import ModalCreateTweet from "./Components/Modals/Modal-Create-Tweet";
 import { NavLink } from "react-router-dom";
 import { BsCoin } from "react-icons/bs";
-import News from "./Components/Screens/News.jsx";
+
+import { AiFillHome } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
 
 function Sidebar() {
-	const [showPost, setShowPost] = useState(false);
-	return (
-		<>
-			<div className="sidebar-container">
-				<div className="sidebar">
-					<div className="sidebar-container-mobile">
-						<img
-							className="sidebar-profile-photo"
-							src={Logo}
-							alt={"Shill Site"}
-							width="80px"
-						/>
-						<NavLink to="/">
-							<SidebarOption
-								className="side-bar-icon"
-								active
-								Icon={<HomeIcon />}
-								text="$hill"
-								image={Logo}
-								src="https://freeimage.host/i/HFl919t"
-							/>
-						</NavLink>
-						<NavLink to="/profile">
-							<SidebarOption Icon={<PermIdentityIcon />} text="Profile" />{" "}
-						</NavLink>
+  const [showPost, setShowPost] = useState(false);
+  return (
+    <>
+      <div className="sidebar-container  flex ">
+        <div className="sidebar">
+          <div className="sidebar-container-mobile">
+            <img
+              className="sidebar-profile-photo"
+              src={Logo}
+              alt={"Shill Site"}
+              width="80px"
+            />
 
-						<NavLink to="/crypto" className="sidebar-hide-icon">
-							<SidebarOption Icon={<BsCoin />} text="Crypto" />{" "}
-						</NavLink>
+            <NavLink to="/">
+             <section className=" flex flex-row pt-9  ">
+			  <AiFillHome className=" text-green-500 text-xl" /> 
+              <div className="  text-lg "> Home</div>
+			  </section>
+            </NavLink>
+            <NavLink to="/profile">
+			<section className=" flex flex-row pt-9 ">
+              <CgProfile className=" text-green-500 text-xl" />
+              <div className="text-xl ">Profile </div>
+			  </section>
+            </NavLink>
+{/* 	
+            <img
+              className="md:sm w-8  md:lg:hidden flex center"
+              src={Logo}
+              alt={"Shill Site"}
+              width="80px"
+            /> */}
 
-						<NavLink to="/news">
-							<SidebarOption Icon={<ListAltIcon />} text="News" />{" "}
-						</NavLink>
-					</div>
+            <NavLink to="/crypto" className="sidebar-hide-icon">
+			<section className=" flex flex-row pt-9">
+              <BsCoin className=" text-green-500 text-xl" />
+              <div className=" text-lg"> Crypto</div>
+			  </section>
+            </NavLink>
 
-					{/* this is the 'tweet' button */}
-					<div className="sidebar-post-button">
-						<Button
-							variant="outlined"
-							className="sidebar_Tweet"
-							fullWidth
-							onClick={() => {
-								console.log("clicked");
-								setShowPost(true);
-								document.querySelector(".sidebar-container").style.zIndex = 1;
-							}}>
-							Post
-						</Button>
-						<ModalCreateTweet
-							onClose={() => {
-								setShowPost(false);
-								document.querySelector(".sidebar-container").style.zIndex = 0;
-							}}
-							show={showPost}
-						/>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+            <NavLink to="/news">
+			<section className=" flex flex-row  pt-9">
+              <ListAltIcon className=" text-green-500 text-xl" />
+              <div className="text-lg "> News</div>
+			  </section>
+            </NavLink>
+          </div>
+
+          {/* this is the 'tweet' button */}
+          <div className="sidebar-post-button">
+            <Button
+              variant="outlined"
+              className="sidebar_Tweet"
+              fullWidth
+              onClick={() => {
+                console.log("clicked");
+                setShowPost(true);
+                document.querySelector(".sidebar-container").style.zIndex = 1;
+              }}
+            >
+              Post
+            </Button>
+            <ModalCreateTweet
+              onClose={() => {
+                setShowPost(false);
+                document.querySelector(".sidebar-container").style.zIndex = 0;
+              }}
+              show={showPost}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Sidebar;
